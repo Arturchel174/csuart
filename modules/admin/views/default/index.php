160e81130677ca6faafb6e31c0d06d8c3046af69
Arturoooooo <35744158+Arturchel174@users.noreply.github.com>
2018-05-23 22:31:57 +0500

    Update index.php

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -3,7 +3,6 @@ use yii\helpers\Url;
 /* @var $this yii\web\View */
 
 
-
 $this->title = 'Панель';
 ?>
 <div class="site-index">
@@ -13,13 +12,15 @@ $this->title = 'Панель';
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h1 class="mt-5">Добро пожаловать!</h1>
+                <p class="lead">Научный руководитель:</p>
+				<ul class="list-unstyled " style="margin-top:-15px" >
+			<li>Ольховский Николай</li></ul>
                 <p class="lead">Создатели проекта:</p>
-                <ul class="list-unstyled">
-                    <li>Ольховский Николай</li>
+                <ul class="list-unstyled" style="margin-top:-15px">
                     <li>Хамидулин Артур</li>
                     <li>Твердохлеб Константин</li>
                     <li>Таубенфедер Никита</li>
-                    <li>Баландин Александр</li>
+                    <li style="text-decoration:line-through";>Баландин Александр</li>
                 </ul>
             </div>
         </div>
